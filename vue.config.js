f4a4a2205817b304f349cae934653c523a605cdf
nvieira2 <natalievieira36@gmail.com>
2024-01-08 20:54:55 -0300

    build

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,6 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  pages: {
-    index: {
-      entry: 'src/main.js',
-    },
-  },
+  
   transpileDependencies: true,
 });
