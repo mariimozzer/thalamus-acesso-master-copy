285217d3ee222ba0b0e2fa1e7ed24fb737c06b81
Mariana Mozzer Arantes <mozzerm@yahoo.com.br>
2024-02-16 11:08:44 -0300

    att

--- src/main.js
+++ src/main.js
@@ -2,16 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import BootstrapVueNext from 'bootstrap-vue-next'
-import VueMask from '@devindex/vue-mask'; // <-- ADD THIS LINE
-
+import VueMask from '@devindex/vue-mask'; 
+import axios from 'axios';
+import store from './store';
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
-import store from './store'
 
 const app = createApp(App).use(store)
 app.use(router)
 app.use(BootstrapVueNext)
 app.use(VueMask);
-// app.use(PrimeVue);
+
+const isApp1 = 'http://192.168.0.5:8000/api';
+const backendUrl = isApp1 ? 'http://192.168.0.5:8080/api' : 'http://192.168.0.6:8000/api';
+axios.defaults.baseURL = backendUrl;
+
 app.mount('#app')
